7cc4593fb04364680d2b70e85b7672d858f5f085
mikedeleon9 <mikedeleon9@gmail.com>
2025-03-20 11:25:11 -0400

    starting new project

---
+++ note-taking-app/src/App.jsx
@@ -0,0 +1,16 @@
+
+import { IoHomeOutline } from "react-icons/io5";
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+   
+   <IoHomeOutline />
+    </>
+  )
+}
+
+export default App
