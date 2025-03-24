64f4314f4ec8d048296d0d2c90b61ba52050ac7f
mikedeleon9 <mikedeleon9@gmail.com>
2025-03-24 15:48:08 -0400

    added tags done for the day

--- note-taking-app/src/components/Modal.jsx
+++ note-taking-app/src/components/Modal.jsx
@@ -1,24 +1,34 @@
+import { TiTag } from "react-icons/ti";
 
-
-const Modal = ({ isOpen, onClose }) => {
+const Modal = ({ isOpen, onClose, selectedTags, setSelectedTags }) => {
     if (!isOpen) return null; // Don't render anything if modal is closed
 
     const availableTags = ["Cooking", "Dev", "Fitness", "Health", "Personal", "React", "Recipes", "Shopping", "Travel", "TypeScript"];
+
+    const handleTagClick = (tag) => {
+        if (selectedTags.includes(tag)) {
+          setSelectedTags(selectedTags.filter((t) => t !== tag)); // Remove tag
+        } else {
+          setSelectedTags([...selectedTags, tag]); // Add tag
+        }
+      };
   
     return (
-      <div className="modal-overlay border-2 fixed top-0 left-0 w-screen h-screen bg-[#00000080] flex items-center justify-center">
-        <div className="modal-content bg-white w-1/2 h-1/2 shadow-lg flex flex-col items-center">
+      <div className="modal-overlay border-2 fixed top-0 left-0 w-screen h-screen bg-[#00000080] flex items-center justify-center" onClick={onClose}>
+        <div className="modal-content bg-white max-w-2/3 max-h-2/3 shadow-lg flex flex-col items-center justify-center gap-4 p-10" onClick={(e) => e.stopPropagation()}>
           
           <h2>Select All Applicable Tags</h2>
-          <div className="grid grid-cols-3 grid-rows-3 border-2 ">
-          {availableTags.map((tag) => {
-            return(
-                <div className="">
+          <div className="grid grid-cols-3 grid-rows-3  gap-4">
+          {availableTags.map((tag) => {return (
+           
+                <div 
+                key={tag} onClick={() => handleTagClick(tag)} className="p-4 cursor-pointer flex items-center gap-2">
+                <TiTag></TiTag>
                 <p>{tag}</p>
                 </div>
-            )
             
-          }) }
+            
+         ) }) }
           </div>
           <button onClick={onClose}>Save</button>
         </div>
--- note-taking-app/src/components/NoteBody.jsx
+++ note-taking-app/src/components/NoteBody.jsx
@@ -1,20 +1,25 @@
 
 import { useState } from "react";
+import { TiTag } from "react-icons/ti";
 import Modal from "./Modal";
 
 export default function NoteBody({selectedNote}){
     const [isModalOpen, setIsModalOpen] = useState(false)
+    const [selectedTags, setSelectedTags] = useState([]);
 
     return(
         <div className="col-span-5 p-6  w-full">
       {selectedNote ? (
         <div className="flex flex-col gap-8 ">
             <p className="text-2xl font-bold">Note {selectedNote} Content</p>
-            <div>
-                <button onClick={() => setIsModalOpen(true)} className="text-sm">Tags</button>
-                <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+            <div  className="flex items-center gap-2 cursor-pointer gap-8">
+                <div className="flex items-center gap-2 cursor-pointer" onClick={() => setIsModalOpen(true)}>
+                <TiTag></TiTag>
+                <button  className="text-sm cursor-pointer">Tags</button>
+                </div>
+                <p>Selected Tags go here</p>
             </div>
-            
+            <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} selectedTags={selectedTags} setSelectedTags={setSelectedTags} />
             <textarea className="border p-4"  rows="15" cols="55" placeholder="Let your thoughts roam..."></textarea>
             <div>
                 <button className="bg-blue-600 hover:bg-blue-700 text-white px-3 py-1 text-sm rounded-md cursor-pointer">Save Note</button>
