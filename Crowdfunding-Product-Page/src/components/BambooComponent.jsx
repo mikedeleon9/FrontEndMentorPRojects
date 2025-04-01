11a1b37a7a972ad6d093313f6e4c3daa52b942f2
mikedeleon9 <mikedeleon9@gmail.com>
2025-03-31 20:57:13 -0400

    added main component

---
+++ Crowdfunding-Product-Page/src/components/BambooComponent.jsx
@@ -0,0 +1,24 @@
+import Button from "./Button";
+import BookmarkIcon from "../images/icon-bookmark.svg";
+import masterLogo from "../images/logo-mastercraft.svg"
+
+export default function BambooComponent(){
+    return(
+        <div className="rounded-lg p-12 h-full relative bg-white top-[-90px] flex flex-col gap-8">
+            <div className="flex flex-col items-center gap-4">
+                <h2 className="font-bold text-2xl">Mastercraft Bamboo Monitor Riser</h2>
+                <p className="text-DarkGray">A beautiful & handcrafted monitor stand to reduce neck & eye strain.</p>
+            </div>
+            <div className="flex justify-between">
+                <Button content={"Back this project"}/>
+                    
+                        <button className="relative bg-gray-100 ml-auto rounded-full text-right px-14 py-4 text-DarkGray font-medium"><span>Bookmark</span>
+                            <img className="absolute left-0 top-0 mr-6" src={BookmarkIcon} alt="bookmark-icon " />
+                            </button>
+                        
+                   
+            </div>
+            <img className="absolute top-[-10%] left-1/2 transform -translate-x-1/2" src={masterLogo} alt="master-logo" />
+        </div>
+    )
+}
---
+++ Crowdfunding-Product-Page/src/components/Button.jsx
@@ -0,0 +1,8 @@
+
+export default function Button({content}){
+    return(
+        <button className="bg-ModerateCyan py-3 px-6 rounded-full text-white font-semibold">
+            {content}
+        </button>
+    )
+}
