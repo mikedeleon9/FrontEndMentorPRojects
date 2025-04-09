d9fb66a078b3e36a73524dfed80f97183acd74e2
mikedeleon9 <mikedeleon9@gmail.com>
2025-04-08 22:47:19 -0400

    done for the night

--- Crowdfunding-Product-Page/src/components/ModalPledgeComponent.jsx
+++ Crowdfunding-Product-Page/src/components/ModalPledgeComponent.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 function ModalPledgeComponent({labelTitle, description}) {
   return (
-    <div className='border-gray-200 border-1 p-6 rounded-lg'>
-      <input id={labelTitle} type="radio" />
-      <label htmlFor={labelTitle}>{labelTitle}</label>
-      <p>{description}</p>
+    <div className='border-gray-200 border-1 py-9 px-6 rounded-lg flex flex-col gap-4'>
+        <div className='flex items-center gap-2'>
+      <input  className="w-5 h-5 text-ModerateCyan cursor-pointer accent-ModerateCyan" id={labelTitle} type="radio" />
+      <label className='font-bold' htmlFor={labelTitle}>{labelTitle}</label>
+      </div>
+      <p className='text-DarkGray text-base/7'>{description}</p>
     </div>
   )
 }
