4068f8b150e7fccd8e4e45ad00ca861f917eface
mikedeleon9 <mikedeleon9@gmail.com>
2025-04-12 23:44:37 -0400

    done for the day

--- game/src/components/Grid.jsx
+++ game/src/components/Grid.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react"
 import data from "./data"
+import React from "react";
 
 export default function Grid(){
 
     const [shuffleData, setShuffleData] = useState(data);
-    const [isClicked, setisClicked] = useState("");
+    const [clickedBoxes, setClickedBoxes] = useState([]);
+    const [correctGuess, setCorrectGuess] = useState(false);
 
 
     function shuffleCards(){
@@ -14,9 +16,24 @@ export default function Grid(){
 
     }
 
-    function clickedCard(){
-        setisClicked(prev => !prev);
-      
+  
+
+    function clickedCard(item){
+        setClickedBoxes(prev => {
+
+            const alreadyClicked = prev.find(box => box.name !== item.name);
+
+            if(prev.includes(item)){
+                return prev.filter(n => n !== item);
+            }
+            if (prev.length >= 4) {
+                return prev;
+            }
+            else {
+                return [...prev, name]
+            }
+        });
+        
     
     }
 
@@ -24,8 +41,9 @@ export default function Grid(){
         <div className="flex flex-col items-center gap-4">
         <div className="grid grid-cols-4 grid-rows-4 gap-2">
             {shuffleData.map((item, index) => {
+                const isClicked = clickedBoxes.includes(item.name);
                 return(
-                    <div onClick={clickedCard} className={`${isClicked === item.name ? 'bg-gray-500' : 'bg-white'} border-2 p-2 flex items-center justify-center`}>
+                    <div onClick={() => clickedCard(item.name)} className={`${isClicked ? 'bg-gray-500 text-white' : 'bg-white'} border-2 p-2 flex items-center justify-center`}>
                         <p>{item.name}</p>
                         
                     </div>
