bed8d81d2b5cea16bfb4ca335525a0d37b40749f
mikedeleon9 <mikedeleon9@gmail.com>
2025-04-13 01:18:37 -0400

    hpefully now

--- BrowserExensionProject/vite.config.js
+++ BrowserExensionProject/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/FrontEndMentorPRojects/BrowserExtensionProject/',
+  base: '/FrontEndMentorPRojects/BrowserExensionProject/',
   plugins: [react(), tailwindcss()],
 })
