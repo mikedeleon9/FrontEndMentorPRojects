3920891a1ac66cab79853ee51dc9cd2a80717ef7
mikedeleon9 <mikedeleon9@gmail.com>
2025-04-13 21:43:40 -0400

    fixing crowdfunding page

--- Crowdfunding-Product-Page/src/components/Navbar.jsx
+++ Crowdfunding-Product-Page/src/components/Navbar.jsx
@@ -1,13 +1,37 @@
+import { useState } from "react"
+import hamburgerIcon from "../images/icon-hamburger.svg"
+import closeHamburger from "../images/icon-close-menu.svg"
+
 
 export default function Navbar(){
+
+    const [showPopUp, setShowPopUp] = useState(false);
+
     return (
-        <div className="flex items-center justify-between px-36 z-5 absolute w-full top-8 ">
+        <div className="w-full ">
+        <div className="flex items-center justify-between px-10 md:px-36 z-5 absolute w-full top-8 ">
             <h1 className="text-white text-3xl font-semibold">crowdfund</h1>
-            <ul className="flex items-center gap-12 font-semibold">
+           
+            <img onClick={() => setShowPopUp(!showPopUp)} className="md:hidden cursor-pointer" src={showPopUp ? closeHamburger : hamburgerIcon} alt="hamburger-icon" />
+            <ul className="hidden md:flex flex items-center gap-12 font-semibold">
                 <li className="cursor-pointer text-white hover:text-ModerateCyan">About</li>
                 <li className="cursor-pointer text-white hover:text-ModerateCyan">Discover</li>
                 <li className="cursor-pointer text-white hover:text-ModerateCyan">Get Started</li>
             </ul>
         </div>
+        {showPopUp && (
+        <div className="md:hidden popUp-div absolute w-full top-18 h-58 z-10" >
+                <ul className="flex font-semibold bg-white flex-col justify-evenly z-10 h-full mx-10 rounded-lg">
+                    <li className="px-6 ">About</li>
+                    <hr className="text-slate-200" />
+                    <li className="px-6 ">Discover</li>
+                    <hr className="text-slate-200" />
+                    <li className="px-6 ">Get Started</li>
+                </ul>
+            </div>
+        )}
+        </div>
+        
+
     )
 }
